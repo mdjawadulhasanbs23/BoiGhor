515210d3c3915cb67fd37952aae412bbfa069e23
jawadbs23 <jawadul.hasan@brainstation-23.com>
2023-04-16 16:38:12 +0600

    Validation Added

--- BLL/DTOs/BookDTO.cs
+++ BLL/DTOs/BookDTO.cs
@@ -14,10 +14,12 @@ namespace BLL.DTOs
 
         public int Id { get; set; }
 
-
+        [Required(ErrorMessage = "Please enter the title.")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Title must be between 3 and 100 characters.")]
         public string? Title { get; set; }
 
-
+        [Required(ErrorMessage = "Please enter the language.")]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Language must be between 2 and 20 characters.")]
         public string? Language { get; set; }
 
         public string? CoverImageUrl { get; set; }
